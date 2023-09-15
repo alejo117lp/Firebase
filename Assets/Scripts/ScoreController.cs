6ab4932ea0f4e86110ad0b20659cdc73c6d82be9
Alejo Lopez <alejo-197@outlook.com>
2023-09-15 11:57:59 -0500

    Score Controller

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -7,49 +7,50 @@ using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
-    DatabaseReference mDatabase;
-    string userId;
-    int score;
-
-    private void Start() {
-        mDatabase = FirebaseDatabase.DefaultInstance.RootReference;
-        userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
-    }
-
-    public void WriteNewScore(int score) {
-        mDatabase.Child("users").Child(userId).Child("score").SetValueAsync(score);
-    }
-
-    public void GetUsersScore() {
-        FirebaseDatabase.DefaultInstance.GetReference("/users" + userId + "/username")
-            .GetValueAsync().ContinueWithOnMainThread(task => {
-                if (task.IsFaulted) {
+    DatabaseReference mDatabase; 
+    string UserId; int score = 0;    
+    void Start()    {        
+        mDatabase = FirebaseDatabase.DefaultInstance.RootReference;       
+        UserId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;     
+        GetUserScore();    
+    }    
+    public void WriteNewScore(int score){     
+        mDatabase.Child("users").Child(UserId).Child("score").SetValueAsync(score);
+    }   
+    public void GetUserScore(){   
+        FirebaseDatabase.DefaultInstance            
+            .GetReference("users/"+UserId+"/score")            
+            .GetValueAsync().ContinueWithOnMainThread(task => { 
+                if (task.IsFaulted)                {  
                     Debug.Log(task.Exception);
-                }
-                else if (task.IsCompleted) {
+                }                
+                else if (task.IsCompleted){   
                     DataSnapshot snapshot = task.Result;
-                    Debug.Log("Score: " + snapshot.Value);
-                    
-                }
-            });
-    }
-
-    public void GetUsersHighestScores() {
-        FirebaseDatabase.DefaultInstance.GetReference("users").
-            OrderByChild("score").LimitToLast(5).GetValueAsync().ContinueWithOnMainThread(task =>{
-                if (task.IsFaulted) {
-                    Debug.Log(task.Exception);
-                }
-                else if (task.IsCompleted) {
-                    DataSnapshot snapshot = task.Result;
-                    Debug.Log("Leaders: " + snapshot.Value);
-                }
-            });
-    }
-
-    public void IncrementScore() {
-        score += 100;
-        WriteNewScore(score);
+                    Debug.Log("Score: "+snapshot.Value);  
+                    score = (int)snapshot.Value; 
+                    GameObject.Find("LabelScore").GetComponent<TMPro.TMP_Text>().text = "Score: "+score;  
+                }            });
+    }    
+    public void GetUsersHighestScores()    {   
+        FirebaseDatabase.DefaultInstance            
+            .GetReference("users").OrderByChild("score").LimitToLast(5)            
+            .GetValueAsync().ContinueWithOnMainThread(task =>{ 
+                if (task.IsFaulted){ 
+                    Debug.Log(task.Exception); 
+                }                
+                else if (task.IsCompleted){  
+                    DataSnapshot snapshot = task.Result;  
+                    foreach (var userDoc in (Dictionary<string, object>)snapshot.Value)                    { 
+                        var userObject = (Dictionary<string, object>)userDoc.Value; 
+                        Debug.Log(userObject["username"] + " : " + userObject["score"]); 
+                    }                
+                }            
+            }); 
+    }       
+    public void IncrementScore()    {  
+        score += 100;       
+        GameObject.Find("LabelScore").GetComponent<TMPro.TMP_Text>().text = "Score: " + score;        
+        WriteNewScore(score);    
     }
 }
 
